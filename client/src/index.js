652de34ed6452346ea620c495fc1e6dc592771e6
Ramon Liao <ramonliao.yc@gmail.com>
2023-05-17 20:14:31 +0100

    update build

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "development") {
   const dotenv = require("dotenv");
