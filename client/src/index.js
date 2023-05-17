28857bf62a0210ef15a14674741ba86b5792f809
Ramon Liao <ramonliao.yc@gmail.com>
2023-05-17 23:15:02 +0100

    update client

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-// if (process.env.NODE_ENV === "development") {
-//   const dotenv = require("dotenv");
-//   dotenv.config();
-// }
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
